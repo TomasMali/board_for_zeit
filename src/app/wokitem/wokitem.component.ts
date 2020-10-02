e8b3ccb7a21ff39876f14a9671083c0e997c1d4b
TomasMali <megatoys92@gmail.com>
2020-10-03 00:42:29 +0200

    fix copy function

--- src/app/wokitem/wokitem.component.ts
+++ src/app/wokitem/wokitem.component.ts
@@ -12,6 +12,8 @@ export class WokitemComponent implements OnInit {
 
   @Input() workitem: Workitem
 
+  workitem_: Workitem
+
   color: string = "Lavender"
 
   constructor(private WIService: WorkitemService) {
@@ -21,23 +23,23 @@ export class WokitemComponent implements OnInit {
   ngOnInit() {
 
 
-this.colors = [
-  { name: 'Lavender' },
-  { name: 'yellow' },
-  { name: 'GreenYellow' },
-  { name: 'Lavender' },
-  { name: "LightSkyBlue" },
-  { name: "DarkOrange" },
-  { name: "Orange" }
-]
+    this.colors = [
+      { name: 'Lavender' },
+      { name: 'yellow' },
+      { name: 'GreenYellow' },
+      { name: 'Lavender' },
+      { name: "LightSkyBlue" },
+      { name: "DarkOrange" },
+      { name: "Orange" }
+    ]
 
-this.states = [
-  { name: 'N' },
-  { name: 'S' },
-  { name: 'T' },
-  { name: 'D' }
+    this.states = [
+      { name: 'N' },
+      { name: 'S' },
+      { name: 'T' },
+      { name: 'D' }
 
-]
+    ]
 
     this.selectedState = this.workitem.state
     this.selectedColor = this.workitem.color
@@ -100,8 +102,8 @@ this.states = [
     }
 
     this.WIService.changeColorOrStateWorkItem(workItem)
-        //
-      this.WIService.notifyAll(workItem)
+    //
+    this.WIService.notifyAll(workItem)
 
   }
 
@@ -116,10 +118,57 @@ this.states = [
   }
 
 
-  copy(){
-    if (confirm("Stai per copiare il WI, proseguire?")) {
-      this.WIService.copyWorkI(this.workitem)
+  copy(storyPoints) {
+
+    var wi= {
+      wi: parseInt(this.getCookie("wi")),
+      storyPoint: parseFloat(storyPoints),
+      sprint: this.getCookie("sprint"),
+      description: this.getCookie("description"),
+      state: this.getCookie("state"),
+      color: this.getCookie("color")
     }
+
+
+
+
+    this.WIService.copyWorkI(wi)
+
+  }
+
+
+
+
+  getValue() {
+    var e = (document.getElementById("sel1") as HTMLInputElement).value;
+    return e
+  }
+
+
+
+  chiama(workitem){
+    document.cookie = "wi=" + workitem.wi;
+    document.cookie = "storyPoint=" + workitem.storyPoint;
+    document.cookie = "sprint=" + workitem.sprint;
+    document.cookie = "description=" + workitem.description;
+    document.cookie = "state=" + workitem.state;
+    document.cookie = "color=" + workitem.color;
+  }
+
+  getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
   }
 
 
